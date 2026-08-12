243f86f60594cbbb1612da9647ca280e5f5f6fa5
z0rk1 <true.z0rk1@gmail.com>
2026-08-12 14:06:26 +0300

    Localize notifications, flashes and model errors to Russian

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module HotelCms
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
+    config.i18n.available_locales = %i[ru en]
+    config.i18n.default_locale = :ru
+    config.i18n.fallbacks = [ :en ]
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
